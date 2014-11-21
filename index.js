7e4c88bb818a50e1a85d32614bd5a5d1bb09d638
Alexander Ulizko <alexander@ulizko.com>
2014-11-22 02:33:48 +0400

    Добавил документацию по двум классам

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ getLatestTerritoriesList().
         var currentDate = new Date();
         var threeMonthAgo = new Date();
         threeMonthAgo.setMonth(currentDate.getMonth() - 1);
-        return territories[0].sensors[0].getSeriesOfMeasurements(threeMonthAgo, currentDate);
+        return territories[0].fetchSeriesOfMeasurements(threeMonthAgo, currentDate);
     }).
     then(function(data) {
         Utils.logInspect(data);
